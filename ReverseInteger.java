2d09635d91853cc1aec5f329c7f85eba725bc6a6
Karthikaveni <karthikaveni18@gmail.com>
2017-07-04 21:21:08 +0530

    Create ReverseInteger.java

---
+++ ReverseInteger.java
@@ -0,0 +1,23 @@
+# hji
+import java.io.*;
+import java.util.*;
+public class ReverseInteger{
+  public static void main(String args[])
+  {
+    int num=0;
+    int reverse=0;
+    Scanner sc=new Scanner(System.in);
+    num=sc.nextInt();
+    
+    System.out.println("Input your number and press enter:");
+    while(num!=0)
+    {
+      
+      reverse = reverse * 10;
+      reverse = reverse + num%10;
+      num = num/10;
+    }
+    System.out.println("Reverse of input number is: "+reverse);
+  }
+}
+  
