bf57b774e52a58ad292aa365914c36b4b8e1542f
Karthikaveni <karthikaveni18@gmail.com>
2017-07-06 21:24:30 +0530

    Create ArraySubset.java

---
+++ ArraySubset.java
@@ -0,0 +1,23 @@
+# hji
+import java.util.*;
+public class ArraySubset
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    ArrayList<Integer> list1=new ArrayList<Integer>();
+    ArrayList<Integer> list2=new ArrayList<Integer>();
+    for(int i=0;i<n;i++)
+    list1.add(sc.nextInt());
+    n=sc.nextInt();
+    for(int i=0;i<n;i++)
+    list2.add(sc.nextInt());
+    if(list1.size()==0 || list2.size()==0)
+    {
+      System.out.println("Input data is insufficient");
+      return;
+    }
+    System.out.println(list1.containsAll(list2));
+  }
+}
